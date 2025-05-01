a7beb3e4acfebe02b29e2137b6a26fc38f7e11b2
Pax <papaxlord@gmail.com>
2025-05-02 01:42:57 +0200

    feat: add fmod and fskl detection

--- rsfrontier-core/src/magic/mod.rs
+++ rsfrontier-core/src/magic/mod.rs
@@ -16,6 +16,46 @@ pub fn get_extension(magic: u32) -> Option<&'static str> {
         .map(|&(_, ext)| ext)
 }
 
+pub fn is_file_fmod(buf: &[u8]) -> bool {
+    let header = u32::from_le_bytes(buf[0..4].try_into().unwrap());
+    if header != 1 {
+        return false;
+    }
+
+    let file_len = u32::from_le_bytes(buf[8..12].try_into().unwrap());
+    if file_len != buf.len() as u32 {
+        return false;
+    }
+
+    true
+}
+
+pub fn is_file_fskl(buf: &[u8]) -> bool {
+    let header = u32::from_le_bytes(buf[0..4].try_into().unwrap());
+    if header != 0xC0000000 {
+        return false;
+    }
+
+    let file_len = u32::from_le_bytes(buf[8..12].try_into().unwrap());
+    if file_len != buf.len() as u32 {
+        return false;
+    }
+
+    true
+}
+
 pub fn find_buf_extension(buf: &[u8]) -> &str {
-    get_extension(u32::from_le_bytes(buf[0..4].try_into().unwrap())).unwrap_or("bin")
+    if let Some(ext) = get_extension(u32::from_le_bytes(buf[0..4].try_into().unwrap())) {
+        return ext;
+    }
+
+    if is_file_fskl(buf) {
+        return "fskl";
+    }
+
+    if is_file_fmod(buf) {
+        return "fmod";
+    }
+
+    "bin"
 }
